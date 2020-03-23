62f560edad5c3713cf1ce0beb7338ed32c537ea5
anu radha <anu.sivasubramanian@gmail.com>
2020-03-23 16:04:20 +0100

    quest 020

--- front/homer_quests/src/components/SignUp.js
+++ front/homer_quests/src/components/SignUp.js
@@ -2,18 +2,61 @@ import React, { Component } from "react";
 
 class SignUp extends Component {
   state = {
-    email: "test@test.com"
+    email: "mon@email.com",
+    password: "monPassw0rd",
+    passwordConfirm: "monPassw0rd",
+    name: "James",
+    lastname: "Bond"
   };
   updateEmailField = e => {
     this.setState({
       email: e.target.value
     });
   };
+  updatePwdField = e => {
+    this.setState({
+      password: e.target.value
+    });
+  };
+  updatePwdConfirmField = e => {
+    this.setState({
+      passwordConfirm: e.target.value
+    });
+  };
+  updateNameField = e => {
+    this.setState({
+      name: e.target.value
+    });
+  };
+  updateLastnameField = e => {
+    this.setState({
+      lastname: e.target.value
+    });
+  };
+  handleSubmit = () => {
+    console.log("to test" + this.state);
+  };
+
   render() {
     return (
       <div>
-        <h1>{this.state.email}</h1>
-        <input type="email" name="email" onChange={this.updateEmailField} />
+        <h1>{JSON.stringify(this.state, 1, 1)}</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input type="email" name="email" onChange={this.updateEmailField} />
+          <input type="text" name="password" onChange={this.updatePwdField} />
+          <input
+            type="text"
+            name="password_confirm"
+            onChange={this.updatePwdConfirmField}
+          />
+          <input type="text" name="name" onChange={this.updateNameField} />
+          <input
+            type="text"
+            name="lastname"
+            onChange={this.updateLastnameField}
+          />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
