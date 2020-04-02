4c4066bd4596e1c9695a5208a44ee378ccd4a951
anu radha <anu.sivasubramanian@gmail.com>
2020-04-02 17:24:09 +0200

    updated

--- back/helpers/db.js
+++ back/helpers/db.js
@@ -1,8 +1,8 @@
-const mysql      = require('mysql');
+const mysql = require("mysql");
 const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '5973',
-  database : 'my_db'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
-module.exports  =  connection;
+module.exports = connection;
